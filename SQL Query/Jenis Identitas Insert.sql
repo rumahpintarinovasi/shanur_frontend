a82b11ba107f4d918569f8523f986db055b27e00
Brian Thomas <56766194+brianbct129@users.noreply.github.com>
2023-11-29 19:02:54 +0800

    feat: add new templates
    - Backend Node.js
    - Frontend Vue.js

--- SQL Query/Jenis Identitas Insert.sql
+++
@@ -1,7 +0,0 @@
-/*
--- Query: SELECT * FROM epos.jenisidentitas
--- Date: 2023-08-10 10:36
-*/
-INSERT INTO `jenisidentitas` (`KodeJenis`,`NamaJenis`) VALUES (1,'KTP');
-INSERT INTO `jenisidentitas` (`KodeJenis`,`NamaJenis`) VALUES (2,'SIM');
-INSERT INTO `jenisidentitas` (`KodeJenis`,`NamaJenis`) VALUES (3,'Kartu Mahasiswa');
--- SQL Query/Sift Insert.sql
+++
@@ -1,8 +0,0 @@
-/*
--- Query: SELECT * FROM epos.sift
--- Date: 2023-08-10 10:32
-*/
-INSERT INTO `sift` (`KodeSift`,`Sift`,`WaktuMulai`,`WaktuSelesai`) VALUES (2,1,'08:00:00','12:00:00');
-INSERT INTO `sift` (`KodeSift`,`Sift`,`WaktuMulai`,`WaktuSelesai`) VALUES (3,2,'12:00:00','16:00:00');
-INSERT INTO `sift` (`KodeSift`,`Sift`,`WaktuMulai`,`WaktuSelesai`) VALUES (4,3,'16:00:00','20:00:00');
-INSERT INTO `sift` (`KodeSift`,`Sift`,`WaktuMulai`,`WaktuSelesai`) VALUES (5,4,'20:00:00','23:59:59');
