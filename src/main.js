1ed21485bd1a4467543e46bde68018fe641f960c
TrainzCoder <prawira206jng@gmail.com>
2024-01-13 21:13:06 +0800

    fix init template nuxt 3

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import router from './routes'
-import App from './App.vue'
-import "bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-createApp(App).use(router).mount('#app')
-// createApp(App).use(router)
-// createApp(App).mount('#app')
