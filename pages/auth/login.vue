56e8cdf942c760f132bcf61eeafc7dcf16ecf23d
Brian Thomas <56766194+brianbct129@users.noreply.github.com>
2024-02-07 17:17:06 +0800

    style(#shn-38): remove code in login form

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -28,47 +28,17 @@
             <!-- /.checkbox -->
           </div>
           <!-- /.pull-left -->
-          <div class="pull-right">
-            <a href="page-recoverpw.html" class="a-link"
-              ><i class="fa fa-unlock-alt"></i>Forgot password?</a
-            >
-          </div>
-          <!-- /.pull-right -->
         </div>
         <!-- /.clearfix -->
         <button type="submit" class="frm-submit">
           Login<i class="fa fa-arrow-circle-right"></i>
         </button>
         <div class="row small-spacing">
-          <div class="col-sm-12">
-            <div class="txt-login-with txt-center">or login with</div>
-            <!-- /.txt-login-with -->
-          </div>
-          <!-- /.col-sm-12 -->
-          <div class="col-sm-6">
-            <button
-              type="button"
-              class="btn btn-sm btn-icon btn-icon-left btn-social-with-text btn-facebook text-white waves-effect waves-light"
-            >
-              <i class="ico fa fa-facebook"></i><span>Facebook</span>
-            </button>
-          </div>
-          <!-- /.col-sm-6 -->
-          <div class="col-sm-6">
-            <button
-              type="button"
-              class="btn btn-sm btn-icon btn-icon-left btn-social-with-text btn-google-plus text-white waves-effect waves-light"
-            >
-              <i class="ico fa fa-google-plus"></i>Google+
-            </button>
-          </div>
-          <!-- /.col-sm-6 -->
+         
         </div>
         <!-- /.row -->
-        <a href="/auth/register" class="a-link"
-          ><i class="fa fa-sign-in"></i>New to Shanur? Register..</a
-        >
-        <div class="frm-footer">SHANUR 2022.</div>
+        
+        <div class="frm-footer text-center">SHANUR 2022.</div>
         <!-- /.footer -->
       </div>
       <!-- .inside -->
